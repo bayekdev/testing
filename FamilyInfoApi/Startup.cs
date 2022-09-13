5c9d02b5cc0890dc19fc8db5305e0dfb0aa4972b
sohel <nadafsohel>
2022-09-13 16:49:42 +0530

    Bbset Service Configured

--- FamilyInfoApi/Startup.cs
+++ FamilyInfoApi/Startup.cs
@@ -4,20 +4,25 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FamilyInfoApi.Data;
 
 namespace FamilyInfoApi
 {
     public class Startup
     {
+        private readonly string ConnectionString;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
         public IConfiguration Configuration { get; }
@@ -25,7 +30,10 @@ namespace FamilyInfoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // connection Dbcontext class service added
+            services.AddDbContext<FamilyDbContext>(options => options.UseSqlServer(ConnectionString));
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
