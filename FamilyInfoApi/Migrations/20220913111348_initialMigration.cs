cd575d87cea4bc766c9e2bed18c8c4e5241379ab
sohel <nadafsohel>
2022-09-13 16:50:04 +0530

    auto generaed code after migration

---
+++ FamilyInfoApi/Migrations/20220913111348_initialMigration.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FamilyInfoApi.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Casts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CastName = table.Column<string>(nullable: true),
+                    CastCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Casts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenderType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaritialStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaritialStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "infos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 250, nullable: false),
+                    MiddleName = table.Column<string>(maxLength: 250, nullable: false),
+                    LastName = table.Column<string>(maxLength: 250, nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    GenderId = table.Column<int>(nullable: false),
+                    CastId = table.Column<int>(nullable: false),
+                    MaritialId = table.Column<int>(nullable: false),
+                    MobileNo = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_infos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_infos_Casts_CastId",
+                        column: x => x.CastId,
+                        principalTable: "Casts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_infos_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_infos_MaritialStatuses_MaritialId",
+                        column: x => x.MaritialId,
+                        principalTable: "MaritialStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_infos_CastId",
+                table: "infos",
+                column: "CastId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_infos_GenderId",
+                table: "infos",
+                column: "GenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_infos_MaritialId",
+                table: "infos",
+                column: "MaritialId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "infos");
+
+            migrationBuilder.DropTable(
+                name: "Casts");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+
+            migrationBuilder.DropTable(
+                name: "MaritialStatuses");
+        }
+    }
+}
