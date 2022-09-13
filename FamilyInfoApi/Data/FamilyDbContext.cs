d372b25004bf80dd99b730ee431490c8840e6eb6
sohel <nadafsohel>
2022-09-13 16:19:50 +0530

    Add project files.

---
+++ FamilyInfoApi/Data/FamilyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FamilyInfoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyInfoApi.Data
+{
+    public class FamilyDbContext:DbContext
+    {
+        public FamilyDbContext(DbContextOptions<FamilyDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Info> infos { get; set; }
+    }
+}
---
+++ FamilyInfoApi/Models/Info.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyInfoApi.Models
+{
+    public class Info
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string MiddleName { get; set; }
+        [MaxLength(250)]
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public int Gender { get; set; }
+        [Required]
+        public string Cast { get; set; }
+        [Required]
+        public string MaritalStatus { get; set; }
+        [Required]
+        public int MobileNo { get; set; }
+    }
+}
